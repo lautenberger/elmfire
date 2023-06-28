4699f95777e9e44bebfee38d0ea2589ae8d94a10
lautenberger <chris@cloudfire.ai>
2023-06-28 22:31:54 +0000

    Set NODATA to 255 for crown fire type tifs

--- runs/forecasts/templates/active_fires/03-make_tifs.sh
+++ runs/forecasts/templates/active_fires/03-make_tifs.sh
@@ -1,37 +1,25 @@
 #!/bin/bash
 
 SRS=`gdalsrsinfo ./asp.tif  | grep "PROJ.4" | cut -d':' -f2 | xargs`
-#SOCKETS=`lscpu | grep 'Socket(s)' | cut -d: -f2 | xargs`
-#CORES_PER_SOCKET=`lscpu | grep 'Core(s) per socket' | cut -d: -f2 | xargs`
-#let "N = SOCKETS * CORES_PER_SOCKET"
 
 compress () {
    local f=$1
    local OT=$2
+   local NODATA=$3
    local STUB=`echo $f | cut -d. -f1`
 
-   gdal_translate -a_srs "$SRS" -a_nodata 0 -ot $OT -co "TILED=yes" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" -co "NUM_THREADS=2" $STUB.bil $STUB.tif && \
-   rm -f $STUB.bil $STUB.hdr
+   gdal_translate -a_srs "$SRS" -a_nodata $NODATA -ot $OT -co "TILED=yes" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" -co "NUM_THREADS=2" \
+                   $STUB.bil $STUB.tif && rm -f $STUB.bil $STUB.hdr
 }
 
-#(
-#for f in *.bil; do
-#   ((i=i%N)); ((i++==0)) && wait
-#   compress "$f" &
-#done
-#)
-
-#QUANTITIES='crown-fire flame-length hours-since-burned spread-rate'
-for f in time*.bil              ; do compress "$f" Float32 & done
-for f in crown-fire*.bil        ; do compress "$f" Byte    & done
+for f in time*.bil              ; do compress "$f" Float32 0 & done
+for f in crown-fire*.bil        ; do compress "$f" Byte  255 & done
 wait
-for f in flame-length*.bil      ; do compress "$f" Byte    & done
+for f in flame-length*.bil      ; do compress "$f" Byte    0 & done
 wait
-for f in hours-since-burned*.bil; do compress "$f" Byte    & done
+for f in hours-since-burned*.bil; do compress "$f" Int16   0 & done
 wait
-for f in spread-rate*.bil       ; do compress "$f" Byte    & done
-wait
-
+for f in spread-rate*.bil       ; do compress "$f" Byte    0 & done
 wait
 
 exit 0
