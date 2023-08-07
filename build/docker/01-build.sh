2fca022502c2be249b7f59348a60374db15ace9e
lautenberger <chris@cloudfire.ai>
2023-08-07 13:53:33 +0000

    Remove cloudfire dependencies

--- build/docker/01-build.sh
+++ build/docker/01-build.sh
@@ -2,18 +2,13 @@
 
 CWD=$(pwd)
 ELMFIRE_CLEAN=$HOME/elmfire_clean
-CLOUDFIRE_CLEAN=$HOME/cloudfire_clean
 
 rm -f -r $CWD/temp
-mkdir -p $CWD/temp/elmfire $CWD/temp/cloudfire
+mkdir -p $CWD/temp/elmfire
 
 cd $ELMFIRE_CLEAN
 git pull
 cp -f -r * $CWD/temp/elmfire/
-
-cd $CLOUDFIRE_CLEAN
-git pull
-cp -f -r * $CWD/temp/cloudfire/
 
 cd $CWD
 docker build -t elmfire .
