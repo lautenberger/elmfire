afa73d29166bb9706afdc7187f927d5e27eee069
lautenberger <chris@cloudfire.ai>
2023-10-15 23:05:00 +0000

    Increment version number

--- build/linux/make_intel.sh
+++ build/linux/make_intel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # ELMFIRE version:
-ELMFIRE_VER=2023.1001
+ELMFIRE_VER=2023.1015
 
 # ELMFIRE uses several environment variables for compilation. If the default
 # values specified on lines 14 - 16 below are not appropriate for your system,
