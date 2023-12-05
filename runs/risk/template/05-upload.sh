505362deab19ac541720713bee3202e805d9d549
lautenberger <chris@cloudfire.ai>
2023-12-05 16:47:25 +0000

    Update remote directory structure

--- runs/risk/template/05-upload.sh
+++ runs/risk/template/05-upload.sh
@@ -37,11 +37,11 @@ if [ "$REMOTE_HOST" = "none" ]; then
 fi
 
 # Start with times burned & split lines
-ssh $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_DIRECTORY; sudo mkdir fire_risk_forecast; sudo mkdir fire_risk_forecast/$FORECAST_CYCLE; sudo mkdir fire_risk_forecast/$FORECAST_CYCLE/elmfire; sudo mkdir fire_risk_forecast/$FORECAST_CYCLE/elmfire/$PATTERN; sudo mkdir fire_risk_forecast/$FORECAST_CYCLE/elmfire/$PATTERN/landfire"
+ssh $REMOTE_USER@$REMOTE_HOST "sudo mkdir -p $REMOTE_DIRECTORY/fire_risk_forecast/$PATTERN/$FORECAST_CYCLE/elmfire/landfire"
 if [ "$REMOTE_OWNER" = "sig-reax" ]; then
-   ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/fire_risk_forecast/$FORECAST_CYCLE; sudo chown -R 'sig-reax:domain users' $REMOTE_DIRECTORY/fire_risk_forecast/$FORECAST_CYCLE"
+   ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/fire_risk_forecast/$PATTERN/$FORECAST_CYCLE; sudo chown -R 'sig-reax:domain users' $REMOTE_DIRECTORY/fire_risk_forecast/$PATTERN/$FORECAST_CYCLE"
 else
-   ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/fire_risk_forecast/$FORECAST_CYCLE; sudo chown -R 'elmfire:domain users'  $REMOTE_DIRECTORY/fire_risk_forecast/$FORECAST_CYCLE"
+   ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/fire_risk_forecast/$PATTERN/$FORECAST_CYCLE; sudo chown -R 'elmfire:domain users'  $REMOTE_DIRECTORY/fire_risk_forecast/$PATTERN/$FORECAST_CYCLE"
 fi
 
 if [ "$PATTERN" != "all" ]; then
@@ -49,26 +49,26 @@ if [ "$PATTERN" != "all" ]; then
 fi
 
 FNLIST=`ls ./out/*.tif ./out/*.shp ./out/*.dbf ./out/*.prj ./out/*.shx`
-scp -r $FNLIST $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIRECTORY/fire_risk_forecast/$FORECAST_CYCLE/elmfire/$PATTERN/landfire/
+scp -r $FNLIST $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIRECTORY/fire_risk_forecast/$PATTERN/$FORECAST_CYCLE/elmfire/landfire/
 
 if [ "$REMOTE_OWNER" = "sig-reax" ]; then
-   ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/fire_risk_forecast/$FORECAST_CYCLE; sudo chown -R 'sig-reax:domain users' $REMOTE_DIRECTORY/fire_risk_forecast/$FORECAST_CYCLE"
+   ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/fire_risk_forecast/$PATTERN/$FORECAST_CYCLE; sudo chown -R 'sig-reax:domain users' $REMOTE_DIRECTORY/fire_risk_forecast/$PATTERN/$FORECAST_CYCLE"
 else
-   ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/fire_risk_forecast/$FORECAST_CYCLE; sudo chown -R 'elmfire:domain users'  $REMOTE_DIRECTORY/fire_risk_forecast/$FORECAST_CYCLE"
+   ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/fire_risk_forecast/$PATTERN/$FORECAST_CYCLE; sudo chown -R 'elmfire:domain users'  $REMOTE_DIRECTORY/fire_risk_forecast/$PATTERN/$FORECAST_CYCLE"
 fi
 
 # Now on to zonal statistics
 if [ "$UPLOAD_PSPS_ZONAL_STATS" = "yes" ]; then
-   ssh $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_DIRECTORY; sudo mkdir psps_zonal; sudo mkdir psps_zonal/$FORECAST_CYCLE; sudo mkdir psps_zonal/$FORECAST_CYCLE/$PATTERN"
-   ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/psps_zonal/$FORECAST_CYCLE; sudo chown -R 'sig-reax:domain users' $REMOTE_DIRECTORY/psps_zonal/$FORECAST_CYCLE"
+   ssh $REMOTE_USER@$REMOTE_HOST "sudo mkdir -p $REMOTE_DIRECTORY/psps_zonal/$PATTERN/FORECAST_CYCLE"
+   ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/psps_zonal/$PATTERN/$FORECAST_CYCLE; sudo chown -R 'sig-reax:domain users' $REMOTE_DIRECTORY/psps_zonal/$PATTERN/$FORECAST_CYCLE"
    FNLIST=`ls ./deenergization-zones/*`
 
-   scp -r $FNLIST $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIRECTORY/psps_zonal/$FORECAST_CYCLE/$PATTERN/
+   scp -r $FNLIST $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIRECTORY/psps_zonal/$PATTERN/$FORECAST_CYCLE/
 
    if [ "$REMOTE_OWNER" = "sig-reax" ]; then
-      ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/psps_zonal/$FORECAST_CYCLE; sudo chown -R 'sig-reax:domain users' $REMOTE_DIRECTORY/psps_zonal/$FORECAST_CYCLE"
+      ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/psps_zonal/$PATTERN/$FORECAST_CYCLE; sudo chown -R 'sig-reax:domain users' $REMOTE_DIRECTORY/psps_zonal/$PATTERN/$FORECAST_CYCLE"
    else
-      ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/psps_zonal/$FORECAST_CYCLE; sudo chown -R 'elmfire:domain users'  $REMOTE_DIRECTORY/psps_zonal/$FORECAST_CYCLE"
+      ssh $REMOTE_USER@$REMOTE_HOST "sudo chmod -R 775 $REMOTE_DIRECTORY/psps_zonal/$PATTERN/$FORECAST_CYCLE; sudo chown -R 'elmfire:domain users'  $REMOTE_DIRECTORY/psps_zonal/$PATTERN/$FORECAST_CYCLE"
    fi
 
 fi
