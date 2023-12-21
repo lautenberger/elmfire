972728e76c3780e157447aac93f6706a27d95a0f
lautenberger <chris@cloudfire.ai>
2023-12-21 22:22:32 +0000

    Increment version number

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'ELMFIRE'
 copyright = '2023, Chris Lautenberger'
 author = 'Chris Lautenberger'
-release = '2023.1202'
+release = '2023.1221'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
