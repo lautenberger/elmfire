d44509134171d13ff7922344c1ef07086262d05d
yqin123 <yqin123@umd.edu>
2024-03-26 13:03:36 -0400

    UMD Spotting Model - Ignition Process Formulation 1. Time-step independent interpretation of ignition probability. 2. Corrected ember arrival time in the Eulerian Scheme

--- verification/04-spotting_eulerian/01-run.sh
+++ verification/04-spotting_eulerian/01-run.sh
@@ -6,29 +6,35 @@ CELLSIZE=30.0 # Grid size in meters
 
 # End inputs specification
 
-ELMFIRE_VER=${ELMFIRE_VER:-2024.0103}
+ELMFIRE_VER=${ELMFIRE_VER:-2023.1015}
 
 SCRATCH=./scratch
 INPUTS=./inputs
 OUTPUTS=./outputs
+MISC=./misc
 
-rm -f -r $SCRATCH $INPUTS $OUTPUTS
-mkdir $INPUTS $SCRATCH $OUTPUTS
+rm -f -r $SCRATCH $INPUTS $OUTPUTS $MISC
+mkdir $INPUTS $SCRATCH $OUTPUTS $MISC
 
-echo $CELLSIZE | python input_generator.py
+cp $ELMFIRE_BASE_DIR/build/source/fuel_models.csv $MISC
+
+echo $CELLSIZE | python3 input_generator.py
 cp elmfire.data.in $INPUTS/elmfire.data
+cp $ELMFIRE_BASE_DIR/build/source/fuel_models.csv $MISC
+cp $ELMFIRE_BASE_DIR/build/source/building_fuel_models.csv $MISC
 A_SRS="EPSG: 32610" # Spatial reference system - UTM Zone 10
 
 # Execute ELMFIRE
-elmfire_debug_$ELMFIRE_VER ./inputs/elmfire.data
+mpirun -np 1 elmfire ./inputs/elmfire.data
+# elmfire_debug ./inputs/elmfire.data
 
 # Postprocess
-for f in ./outputs/*.bil; do
-   gdal_translate -a_srs "$A_SRS" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" $f ./outputs/`basename $f | cut -d. -f1`.tif
-done
-gdal_contour -i 3600 `ls ./outputs/time_of_arrival*.tif` ./outputs/hourly_isochrones.shp
+#for outputs/*.bil; do
+#   gdal_translate -a_srs "$A_SRS" -co "COMPRESS=DEFLATE" -co "ZLEVEL=9" $f ./outputs/`basename $f | cut -d. -f1`.tif
+#done
+#gdal_contour -i 3600 `ls ./outputs/time_of_arrival*.tif` ./outputs/hourly_isochrones.shp
 
 # Clean up and exit:
-rm -f -r ./outputs/*.bil ./outputs/*.hdr $SCRATCH
+#rm -f -r ./outputs/*.bil ./outputs/*.hdr $SCRATCH $MISC
 
 exit 0
---
+++ verification/04-spotting_eulerian/remake_elmfire.sh
@@ -0,0 +1,4 @@
+current_dir=$(pwd)
+cd ~/software/elmfire_public/elmfire_public/build/linux/
+sh make_gnu.sh
+cd $current_dir
