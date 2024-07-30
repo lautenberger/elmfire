bfe9a9506d8cfb91b4866c9b4a082bd09a323b7a
yqin123 <yqin123@umd.edu>
2024-07-30 16:53:51 -0400

    Correction for PIGN=1 case

--- verification/04-spotting_eulerian/remake_elmfire.sh
+++
@@ -1,4 +0,0 @@
-current_dir=$(pwd)
-cd ~/software/elmfire_public/elmfire_public/build/linux/
-sh make_gnu.sh
-cd $current_dir
--- verification/04-spotting_eulerian/tiffview.m
+++
@@ -1,6 +0,0 @@
-clear
-img = imread('outputs/time_of_arrival_0000001_0010002.tif');
-img = sum(img, 3);
-image(img,'CDataMapping','scaled')
-axis equal
-caxis([0 4000])
