e75666b37f87091f64a4498b2334007d45b1f8af
Chris Lautenberger <chris@cloudfire.com>
2025-01-23 12:31:56 +0000

    Update gRPC files

--- cloudfire/available_polygons_pb2.pyi
+++ cloudfire/available_polygons_pb2.pyi
@@ -4,20 +4,20 @@ from typing import ClassVar as _ClassVar, Optional as _Optional
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
+class Request(_message.Message):
+    __slots__ = ("type", "year", "list", "firename")
+    TYPE_FIELD_NUMBER: _ClassVar[int]
+    YEAR_FIELD_NUMBER: _ClassVar[int]
+    LIST_FIELD_NUMBER: _ClassVar[int]
+    FIRENAME_FIELD_NUMBER: _ClassVar[int]
+    type: str
+    year: str
+    list: str
+    firename: str
+    def __init__(self, type: _Optional[str] = ..., year: _Optional[str] = ..., list: _Optional[str] = ..., firename: _Optional[str] = ...) -> None: ...
+
 class Reply(_message.Message):
-    __slots__ = ["available_polygons"]
+    __slots__ = ("available_polygons",)
     AVAILABLE_POLYGONS_FIELD_NUMBER: _ClassVar[int]
     available_polygons: str
     def __init__(self, available_polygons: _Optional[str] = ...) -> None: ...
-
-class Request(_message.Message):
-    __slots__ = ["firename", "list", "type", "year"]
-    FIRENAME_FIELD_NUMBER: _ClassVar[int]
-    LIST_FIELD_NUMBER: _ClassVar[int]
-    TYPE_FIELD_NUMBER: _ClassVar[int]
-    YEAR_FIELD_NUMBER: _ClassVar[int]
-    firename: str
-    list: str
-    type: str
-    year: str
-    def __init__(self, type: _Optional[str] = ..., year: _Optional[str] = ..., list: _Optional[str] = ..., firename: _Optional[str] = ...) -> None: ...
