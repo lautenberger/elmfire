48217ae7b85a26ba36deebe890633e8b9718141f
lautenberger <chris@cloudfire.ai>
2025-01-25 23:32:12 +0000

    Update to LANDFIRE 2.4.0

--- tutorials/04-fire-potential/01-run.sh
+++ tutorials/04-fire-potential/01-run.sh
@@ -4,7 +4,7 @@
 $ELMFIRE_BASE_DIR/cloudfire/fuel_wx_ign.py \
     --do_wx=False --do_ignition=False \
     --center_lon=-120.05 --center_lat=38.13 \
-    --fuel_source='landfire' --fuel_version='2.3.0' \
+    --fuel_source='landfire' --fuel_version='2.4.0' \
     --outdir='./fuel' --name='tutorial04'
 
 WX_INPUTS_FILE=wx.csv
