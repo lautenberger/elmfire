fff363c667bf936870438af7c22e24cca1e7f6d6
Chris Lautenberger <chris@cloudfire.com>
2025-02-24 14:20:43 +0000

    Increment version

--- verification/03-spotting/01-run.sh
+++ verification/03-spotting/01-run.sh
@@ -6,7 +6,7 @@ CELLSIZE=30.0 # Grid size in meters
 
 # End inputs specification
 
-ELMFIRE_VER=${ELMFIRE_VER:-2025.0218}
+ELMFIRE_VER=${ELMFIRE_VER:-2025.0224}
 
 SCRATCH=./scratch
 INPUTS=./inputs
