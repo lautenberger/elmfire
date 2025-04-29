e8cd72cfcc97d4e7f73b8f10e9089aa5d9c50ed8
dwimjpurnomo <38124875+dwimjpurnomo@users.noreply.github.com>
2025-04-29 09:36:24 -0700

    Speed Up (#84)
    * Update elmfire_vars.f90
    
    Speed Up
    
    * Update elmfire_subs.f90
    
    Speed up
    
    * Update elmfire_spread_rate.f90
    
    Speed up
    
    * Update elmfire_level_set.f90
    
    Speed up
    
    * Update Makefile_elmfire
    
    Speed up
    
    * Update elmfire_spread_rate.f90
    
    Speed up

--- build/source/elmfire_subs.f90
+++ build/source/elmfire_subs.f90
@@ -1054,15 +1054,21 @@ INTEGER, INTENT(IN)      :: IX, IY
 REAL, INTENT(IN) :: T
  
 TYPE(NODE), POINTER :: NP
+TYPE(NODE_WRAPPER), ALLOCATABLE :: TEMP(:)  ! Temporary array for resizing DWI_SU
+INTEGER :: N  ! Store the new count DWI_SU
 
 ! If the list is empty
 IF (DL2%NUM_NODES == 0) THEN
    CALL INIT(DL2, IX, IY, T)
+   ALLOCATE(DL2%NODE_POINTERS(1))      ! DWI_SU
+   DL2%NODE_POINTERS(1)%PTR => DL2%HEAD   ! DWI_SU
    RETURN
 END IF
  
 ! Add new element ot the end
 DL2%NUM_NODES = DL2%NUM_NODES + 1
+N = DL2%NUM_NODES  ! Store the new count DWI_SU
+
 NP => DL2%TAIL
 ALLOCATE(DL2%TAIL)
 DL2%TAIL%IX         =  IX
@@ -1076,9 +1082,57 @@ DL2%TAIL%TANSLP2 =  TANSLP2(MAX(MIN(NINT(SLP%R4(IX,IY,1)),90),0))
 
 DL2%TAIL%PREV       => NP
 DL2%TAIL%PREV%NEXT  => DL2%TAIL
+
+! Resize NODE_POINTERS array dynamically DWI_SU
+ALLOCATE(TEMP(N-1))
+TEMP = DL2%NODE_POINTERS
+DEALLOCATE(DL2%NODE_POINTERS)
+ALLOCATE(DL2%NODE_POINTERS(N))
+DL2%NODE_POINTERS(1:N-1) = TEMP
+DEALLOCATE(TEMP) 
+
+! Store the new node in the array DWI_SU
+NULLIFY(DL2%NODE_POINTERS(N)%PTR)
+DL2%NODE_POINTERS(N)%PTR => DL2%TAIL
    
 ! *****************************************************************************   
 END SUBROUTINE APPEND
+! *****************************************************************************
+
+! *****************************************************************************
+SUBROUTINE APPEND_TO_DYNAMIC_ARRAY(IX, IY, N_ROWS, DYNAMIC_ARRAY)
+! *****************************************************************************
+
+    INTEGER, INTENT(IN) :: IX, IY                         ! NEW_VALUES_TO_APPEND
+    INTEGER, INTENT(INOUT) :: N_ROWS   
+    REAL, ALLOCATABLE, INTENT(INOUT), DIMENSION(:,:) :: DYNAMIC_ARRAY  ! Dynamic 2D array to store IX and IY
+
+
+    ! Local temporary array for resizing
+    INTEGER, ALLOCATABLE, DIMENSION(:,:) :: TEMP_ARRAY
+
+    ! Handle the case where the array is unallocated
+    IF (N_ROWS .LE. 1) THEN
+        ALLOCATE(DYNAMIC_ARRAY(1, 2))           ! Allocate first column
+        DYNAMIC_ARRAY(1, 1) = IX
+        DYNAMIC_ARRAY(1, 2) = IY
+        N_ROWS = 1                              ! Set number of rows to 1
+    ELSE
+        ! Allocate a temporary array with one additional column
+        ALLOCATE(TEMP_ARRAY(N_ROWS, 2))
+        TEMP_ARRAY(1:N_ROWS-1, :) = DYNAMIC_ARRAY  ! Copy existing data
+        TEMP_ARRAY(N_ROWS, 1) = IX           ! Add new IX value
+        TEMP_ARRAY(N_ROWS, 2) = IY           ! Add new IY value
+
+        ! Replace the old array with the resized one
+        DEALLOCATE(DYNAMIC_ARRAY)
+        ALLOCATE(DYNAMIC_ARRAY(N_ROWS, 2))
+        DYNAMIC_ARRAY = TEMP_ARRAY
+    END IF
+
+
+! *****************************************************************************
+END SUBROUTINE APPEND_TO_DYNAMIC_ARRAY
 ! *****************************************************************************
 
 ! *****************************************************************************
