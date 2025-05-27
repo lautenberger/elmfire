23a4cbd27fc84b4b194fed48a8d933f7e7c7fdeb
Chris Lautenberger <chris@cloudfire.com>
2025-05-27 02:04:17 +0000

    Increment version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'ELMFIRE'
 copyright = '2025, Chris Lautenberger'
 author = 'Chris Lautenberger'
-release = '2025.0522'
+release = '2025.0526'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
