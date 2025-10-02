b041704fc40342394df62828341a890536f398cc
Chris Lautenberger <chris@cloudfire.com>
2025-10-02 14:51:25 +0000

    Increment version to 2025.1002

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'ELMFIRE'
 copyright = '2025, Chris Lautenberger'
 author = 'Chris Lautenberger'
-release = '2025.1001'
+release = '2025.1002'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- tutorials/02-transient-wind/01-run.sh
+++ tutorials/02-transient-wind/01-run.sh
@@ -26,7 +26,7 @@ WX_INPUTS_FILE=wx.csv
 
 # End inputs specification
 
-ELMFIRE_VER=${ELMFIRE_VER:-2025.1001}
+ELMFIRE_VER=${ELMFIRE_VER:-2025.1002}
 
 . ../functions/functions.sh
 
